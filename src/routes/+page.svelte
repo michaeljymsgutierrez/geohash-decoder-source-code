17ea84601b763e9498fd8fa115c5e3b762556d43
michaeljymsgutierrez <leeez711@gmail.com>
2024-07-16 19:33:01 +0800

    Pushed for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+</script>
+
 <svelte:head>
 	<script async src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 </svelte:head>
+
+<div id="map"></div>
+
+<style>
+	#map {
+		height: 180px;
+	}
+</style>
